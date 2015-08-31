f88973ed4a62783e632941ba9be3d99fad6f33f3
Anton Borries <Anton.Borries@hhu.de>
2015-08-31 15:59:00 +0200

    Setting Screen
    Macht noch nichts

--- app/src/main/java/com/antonborries/pairit/SettingsFragment.java
+++ app/src/main/java/com/antonborries/pairit/SettingsFragment.java
@@ -3,6 +3,7 @@ package com.antonborries.pairit;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.preference.PreferenceFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +12,19 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragment {
 
 
     public SettingsFragment() {
         // Required empty public constructor
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       addPreferencesFromResource(R.xml.preferences);
+    }
+
 
 
     @Override
